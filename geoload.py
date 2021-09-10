8197c091a569ce5f9aa7cf10da1f44d45f0dfd89
Tchain Group <78317339+TchainGroup@users.noreply.github.com>
2021-09-10 01:27:36 -0300

    Update geoload.py

--- geoload.py
+++ geoload.py
@@ -24,7 +24,6 @@ fh = open("where.data")
 count = 0
 for line in fh:
     if count > 200 :
-        print('Retrieved 200 locations, restart to retrieve more')
         break
 
     address = line.strip()
